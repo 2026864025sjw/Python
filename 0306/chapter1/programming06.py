56321cfd31536811c74891375aea7ef4fa24b1c5
2026864025sjw <sky005226@gmail.com>
2026-03-12 14:41:40 +0900

    Add files via upload

---
+++ 0306/chapter1/programming06.py
@@ -0,0 +1,11 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.lt(33)
+t.fd(300)
+t.lt(180)
+t.fd(300)
+
+turtle.mainloop()
---
+++ 0306/chapter1/programming08.py
@@ -0,0 +1,11 @@
+import turtle
+
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.color("blue")
+
+t.fd(100)
+
+turtle.mainloop()
